0ebd6cfb9b9c24807bd11ba85b0f126d883e91d2
Oleg Nechaev <lega911@gmail.com>
2020-07-18 19:28:47 +0200

    vue3 + vite

---
+++ vue/App.vue
@@ -0,0 +1,45 @@
+<script>
+import { ref, computed } from 'vue'
+import Item from './Item.vue'
+export default {
+  components: {
+      Item
+  },
+  setup() {
+    let todos = ref([])
+    let name = ref('')
+    let completed = computed(() => todos.value.filter(i => i.done).length )
+
+    const add = () => {
+      if(!name.value) return;
+      todos.value.push({name: name.value});
+      name.value = '';
+    };
+
+    const remove = (i) => {
+      todos.value.splice(i, 1);
+    };
+
+    return {
+      todos,
+      name,
+      add,
+      remove,
+      completed
+    }
+  }
+}
+</script>
+
+<template>
+  <input type="text" v-model="name" @keyup.enter="add" /> <br/>
+
+  <item v-for="(item, index) in todos" :item="item" @remove="remove(index)"></item>
+
+  <template v-if="todos.length">
+    Completed {{completed}} of {{todos.length}}
+  </template>
+  <template v-else>
+    No tasks yet
+  </template>
+</template>
---
+++ vue/Item.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  props: ['item'],
+  setup(props, {emit}) {
+    const remove = () => emit('remove');
+
+    return {
+      remove
+    };
+  }
+}
+</script>
+
+<template>
+  <div :class="{ inactive: item.done }">
+    <input type="checkbox" v-model="item.done" />
+    {{ item.name }}
+    <button @click="remove">Remove</button>
+  </div>
+</template>
+
+<style scoped>
+  .inactive {text-decoration: line-through; color: gray;}
+</style>
